5b90e70cd88938aa9a7b296f77c66d2a392f12ef
Stephanie Zacharias <stephanie@Stephanies-MacBook-Pro-4.local>
2020-08-22 18:22:17 -0700

    Add item to list complete

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello");
-})
+const items = [];
 
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+  const day = date.getDate();
+  res.render("list", {today: day, newListItems: items});
+});
+
+app.post("/", (req, res) => {
+  const item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
+});
 
 app.listen(3000, () => console.log("Server started on port 3000."));
